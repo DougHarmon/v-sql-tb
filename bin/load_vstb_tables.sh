b6b74f95effc75892766487cc3bc1c688cd90e19
Doug Harmon <hokieswon-github@yahoo.com>
2014-07-11 16:59:32 -0400

    Bug Fix
    pointed file to correct version: load_vstb_tables.sql

--- bin/load_vstb_tables.sh
+++ bin/load_vstb_tables.sh
@@ -5,4 +5,4 @@ export SQLDIR=/home/dbadmin/vstb/sql/merge
 export LOGDIR=/home/dbadmin/vstb/log
 export VSQLDIR=/opt/vertica/bin
 
-$VSQLDIR/vsql -f $SQLDIR/load_vstb.sql > $LOGDIR/load_vstb.${TODAY}.log
+$VSQLDIR/vsql -f $SQLDIR/load_vstb_tables.sql > $LOGDIR/load_vstb_tables.${TODAY}.log
